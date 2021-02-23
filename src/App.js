04f16b787ea76e489ee702715bc37c2dc02694d8
Rudra <rudra4365@gmail.com>
2021-02-24 01:08:35 +0530

    Created first component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-import { ChatEngine } from raect-chat-engine;
+import { ChatEngine } from 'react-chat-engine'; 
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID = ""
+            userName = ""
+            userSecret = ""
+        />
+    );
+}
+
+export default App;
