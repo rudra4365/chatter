1eac39132825c535a212357d3144c86e58869a70
Rudra <rudra4365@gmail.com>
2021-02-24 11:16:39 +0530

    modified src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { ChatEngine } from 'react-chat-engine'; 
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
     return (
         <ChatEngine
             height = "100vh"
-            projectID = ""
-            userName = ""
-            userSecret = ""
+            projectID = "e1bc8a88-4653-4a1a-ad94-225230979799"
+            userName = "rudra4365"
+            userSecret = "123123"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
 }
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,8 @@
+const ChatFeed = (props) => {
+    console.log(props);
+    return (
+        <div>This is it...</div>
+    );
+}
+
+export default ChatFeed;
